023a6f78ead3d402409636439dd3d2cb9d488283
Mahadeen <mahadeenabd@gmail.com>
2025-10-16 00:56:44 +0300

    heroku.

--- app/urls.py
+++ app/urls.py
@@ -21,4 +21,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main_app.urls')), # Mounts main_app's routes at the root URL
     path('clients/', include('django.contrib.auth.urls')),
-]
+    ]
